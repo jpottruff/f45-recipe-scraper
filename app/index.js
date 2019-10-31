58cd01b1d3240125690119aed9fd94f064f07c82
Jeff Pottruff <jeffpottruff@gmail.com>
2019-10-30 22:33:25 -0400

    ADD getThisWeeksMeals function - scraping test data successfully

--- app/index.js
+++ app/index.js
@@ -23,8 +23,11 @@ async function run() {
     await page.waitFor(10*1000);
     console.log('...Waited 10 seconds');
 
-
     //TODO - rest of app
+    // const weeklyData = await getThisWeeksMeals(page);
+    // console.log(weeklyData);
+
+    
 }
 
 async function login(page) {
@@ -44,4 +47,63 @@ async function login(page) {
     console.log('LOGGED IN');
 }
 
-run();
+
+// TEST STUFF
+
+
+async function testScrape() {
+    //Launching non-headless for visual debugging
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
+    // BROWSE TO TEST PAGE
+    const page = await browser.newPage();
+    await page.goto('C:/Users/Jeff/Desktop/eat-shit/f45-recipe-scraper/test-data/test.html');
+
+    // WORKING - GO FROM HERE
+    const weeklyData = await getThisWeeksMeals(page);
+    console.log(weeklyData);
+}
+
+
+async function getThisWeeksMeals(page) {
+    
+    let thisWeeksMeals = await page.evaluate( () => {
+        const MEALS_FOR_WEEK_SELECTOR = 'div.meal > div';
+        
+        const MEAL_SECTION_SELECTOR = '.meal_section';
+        const MEAL_ROW_SELECTOR = '#meal-row';
+    
+        const currentWeeksMeals = document.querySelectorAll(MEALS_FOR_WEEK_SELECTOR);
+        let data = [];
+    
+        for (const row of currentWeeksMeals) {
+    
+            //TODO check to see if it exists to avoid errors
+    
+            sectionData = {
+                sectionHTML: row.querySelector(MEAL_SECTION_SELECTOR).outerHTML
+            }
+            mealsData = {
+                mealsForDayHTML: row.querySelector(MEAL_ROW_SELECTOR).outerHTML
+            }
+    
+            data.push({
+                // rowHTML: row.outerHTML
+                section: sectionData,
+                meals: mealsData
+            })
+        };
+        return data;
+    });
+
+    return thisWeeksMeals;
+}
+
+// END TEST STUFF
+
+
+
+testScrape();
+// run();
